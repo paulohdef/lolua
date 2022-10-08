bfd93537b8b3ba75a8c66790a378d0705515d2ff
Paulo Henrique de Freitas <72558339+paulohdef@users.noreply.github.com>
2022-10-08 01:00:13 -0300

    feat: initiate project

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="bg-pink-2 flex items-center justify-center" elevated>
+          <div>
+            <img src="../assets/logo.png" style="width: 200px">
+           </div>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+export default {
+  name: 'MainLayout',
+  components: {
+
+  },
+  data () {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: linksData
+    }
+  }
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,152 @@
+<template>
+  <q-page class="">
+    <div>
+
+      <q-card>
+        <q-card-section  class="bg-pink-1">
+          <div class="flex items-center justify-center">
+            <span class="text-pink-3 col-12 justify-center items-center flex my-font q-py-md" style="font-size: 42px;">
+              Fatias Gourmet
+            </span>
+          </div>
+          <div class="flex items-center justify-center row">
+            <div class="flex items-center justify-center row col-12 q-pa-lg">
+                <span class="text-pink-6 col-12 justify-center items-center flex my-font2 col-12 q-py-md" style="font-size: 34px;">
+                  Red Velvet
+                </span>
+                <span class="text-pink-3 col-12 justify-center items-center flex my-font3 col-12 q-py-md">
+                  Massa vermelha com recheio de creme cheese frosting
+                </span>
+              </div>
+            <div class="q-mx-xl">
+              <img src="../assets/red-velvet-1.jpeg" style="width: 300px">
+            </div>
+
+            <div class="q-ma-xl">
+              <img src="../assets/red-velvet-2.jpeg" style="width: 300px">
+            </div>
+          </div>
+        </q-card-section>
+      </q-card>
+      <q-card>
+        <q-card-section class="bg-pink-1">
+          <div class="flex items-center justify-center">
+            <span class="text-pink-3 col-12 justify-center items-center flex my-font" style="font-size: 42px;">
+              Bentô Cake
+            </span>
+          </div>
+
+          <div class="row">
+              <div class="flex items-center justify-center row col-12 q-pa-lg">
+                <span class="text-pink-6 col-12 justify-center items-center flex my-font2 col-12 q-py-md" style="font-size: 34px;">
+                  Red Velvet
+                </span>
+                <span class="text-pink-3 col-12 justify-center items-center flex my-font3 col-12 q-py-md">
+                  Massa vermelha com recheio de creme cheese frosting
+                </span>
+              </div>
+              <div class="flex items-center justify-center row col-12 q-pa-lg">
+                <span class="text-pink-6 col-12 justify-center items-center flex my-font2 q-py-md" style="font-size: 34px;">
+                  Brigadeiro
+                </span>
+                <span class="text-pink-3 col-12 justify-center items-center flex my-font3 q-py-md">
+                  Massa branca com recheio de brigadeiro:
+                </span>
+                <span class="text-pink-3 col-12 justify-center items-center flex my-font3 q-py-md">
+                  - Ninho
+                </span>
+                <span class="text-pink-3 col-12 justify-center items-center flex my-font3">
+                  - Tradicional (100% Cacau)
+                </span>
+              </div>
+            </div>
+          <div class="flex items-center justify-center bg-pink-1">
+            <div class="q-ma-xl">
+              <img src="../assets/bolo-3.jpeg" style="width: 300px">
+            </div>
+            <div class="q-ma-xl">
+              <img src="../assets/bento-1.jpeg" style="width: 300px">
+            </div>
+            <div class="q-ma-xl">
+              <img src="../assets/bento-2.jpeg" style="width: 300px">
+            </div>
+            <div class="q-ma-xl">
+              <img src="../assets/bento-3.jpeg" style="width: 300px">
+            </div>
+          </div>
+        </q-card-section>
+      </q-card>
+
+      <q-card>
+        <q-card-section class="bg-pink-1">
+          <div class="flex items-center justify-center">
+            <span class="text-pink-3 col-12 justify-center items-center flex my-font" style="font-size: 42px;">
+              Bolo de pote
+            </span>
+          </div>
+
+          <div class="row">
+              <div class="flex items-center justify-center row col-12 q-pa-lg">
+                <span class="text-pink-6 col-12 justify-center items-center flex my-font2 col-12" style="font-size: 34px;">
+                  Ninho com Nutella
+                </span>
+              </div>
+              <div class="flex items-center justify-center row col-12 q-pa-lg">
+                <span class="text-pink-6 col-12 justify-center flex my-font2" style="font-size: 34px;">
+                  Brigadeiro (100% Cacau)
+                </span>
+              </div>
+              <div class="flex items-center justify-center row col-12 q-pa-lg">
+                <span class="text-pink-6 col-12 justify-center items-center flex my-font2 col-12" style="font-size: 34px;">
+                  Ninho com Morango
+                </span>
+              </div>
+            </div>
+          <div class="flex items-center justify-center bg-pink-1">
+            <div class="q-ma-xl">
+              <img src="../assets/bolo-1.jpeg" style="width: 300px">
+            </div>
+          </div>
+        </q-card-section>
+      </q-card>
+
+
+      <q-card>
+        <q-card-section class="bg-pink-1">
+          <div class="flex items-center justify-center">
+            <span class="text-pink-3 col-12 justify-center items-center flex my-font" style="font-size: 42px;">
+              Bolos Decorados
+            </span>
+          </div>
+
+          <div class="flex items-center justify-center bg-pink-1">
+            <div class="q-ma-xl">
+              <img src="../assets/bolo-2.jpeg" style="width: 300px">
+            </div>
+            <div class="q-ma-xl">
+              <img src="../assets/bolo-coelho.jpeg" style="width: 300px">
+            </div>
+          </div>
+        </q-card-section>
+      </q-card>
+      <q-footer  reveal class="bg-pink-2 flex items-center justify-center">
+        <div class="q-pa-sm flex items-center justify-center">
+          <img src="../assets/wpp.png" alt="" style="width:20px">
+          <span class="my-font-3">
+            (91) 98504-9139
+          </span>
+        </div>
+
+      </q-footer>
+
+
+
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "PageIndex",
+};
+</script>
